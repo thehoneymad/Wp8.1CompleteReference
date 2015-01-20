a893203456feb87cf29a12d158b84505893d81f8
thehoneymad <thehoneymad@hotmail.com>
2015-01-20 15:02:13 +0600

    Tracking geolocation code sample added

---
+++ Maps/Maps And GeoLocation/MapTest/MapTest/LocationData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml;
+
+namespace MapTest
+{
+    public class LocationData: DependencyObject
+    {
+        public Geopoint Location { get; set; }
+        public string Name { get; set; }
+    }
+}
