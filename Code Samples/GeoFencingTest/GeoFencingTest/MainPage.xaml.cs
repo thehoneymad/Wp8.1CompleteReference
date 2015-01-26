ec3a2f3131202d24de3e6a893a39f24e85e2aa2f
thehoneymad <thehoneymad@hotmail.com>
2015-01-26 17:11:30 +0600

    Started to add geofencing sample

---
+++ Code Samples/GeoFencingTest/GeoFencingTest/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace GeoFencingTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Geolocator geolocator = new Geolocator();
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+
+        async private void InitializeGeoFence()
+        {
+            try
+            {
+                // Get a geolocator object 
+                geolocator = new Geolocator();
+
+                // Get cancellation token
+                cts = new CancellationTokenSource();
+                CancellationToken token = cts.Token;
+
+                await geolocator.GetGeopositionAsync().AsTask(token);
+
+                
+
+            }
+            
+            catch (Exception)
+            {
+                if (geolocator.LocationStatus == PositionStatus.Disabled)
+                {
+                    
+                }
+
+            }
+            finally
+            {
+                cts = null;
+            }
+
+        }
+
+        private async void ShowMessage(string message)
+        {
+            MessageDialog dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+    }
+}
