d24b36d1ff36b0f679081dc40f3b4a873eb51f24
Swagata Prateek <thehoneymad@hotmail.com>
2015-01-27 23:22:15 +0600

    Added geofencing code example

--- Code Samples/GeoFencing in Windows Phone 8.1 WinRT/GeoFencingTest/GeoFencingTest/MainPage.xaml.cs
+++ Code Samples/GeoFencing in Windows Phone 8.1 WinRT/GeoFencingTest/GeoFencingTest/MainPage.xaml.cs
@@ -4,9 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -20,13 +25,14 @@ using Windows.UI.Xaml.Navigation;
 
 namespace GeoFencingTest
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
         Geolocator geolocator = new Geolocator();
         CancellationTokenSource CancellationTokenSrc = new CancellationTokenSource();
+
+        Geofence geofence = null;
+        int DefaultDwellTimeInSeconds = 0;
 
         public MainPage()
         {
@@ -39,12 +45,12 @@ namespace GeoFencingTest
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
 
-            InitializeGeoFence();
+            InitializeLocation();
             
         }
 
 
-        async private void InitializeGeoFence()
+        async private void InitializeLocation()
         {
             try
             {
@@ -55,7 +61,11 @@ namespace GeoFencingTest
                 CancellationTokenSrc = new CancellationTokenSource();
                 CancellationToken token = CancellationTokenSrc.Token;
 
-                await geolocator.GetGeopositionAsync(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30)).AsTask(token);
+                var position = await geolocator.GetGeopositionAsync(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30)).AsTask(token);
+
+                Longitude.Text = position.Coordinate.Point.Position.Longitude.ToString();
+                Latitude.Text = position.Coordinate.Point.Position.Latitude.ToString();
+                Altitude.Text = position.Coordinate.Point.Position.Altitude.ToString();
             }
             
             catch (Exception)
@@ -73,10 +83,185 @@ namespace GeoFencingTest
 
         }
 
+
+        private void SetupGeofence()
+        {
+            //Set up a unique key for the geofence
+            string GeofenceKey = "My Home Geofence";
+            BasicGeoposition GeoFenceRootPosition = GetMyHomeLocation();
+
+            double Georadius = 500;
+
+            // the geocircle is the circular region that defines the geofence
+            Geocircle geocircle = new Geocircle(GeoFenceRootPosition, Georadius);
+
+            bool singleUse = (bool)SingleUse.IsChecked;
+
+            //Selecting a subset of the events we need to interact with the geofence
+            MonitoredGeofenceStates GeoFenceStates = 0;
+
+            GeoFenceStates |= MonitoredGeofenceStates.Entered;
+            GeoFenceStates |= MonitoredGeofenceStates.Exited;
+
+            // setting up how long you need to be in geofence for enter event to fire
+            TimeSpan dwellTime;
+
+            dwellTime = DwellTime.Text!=""? ParseTimeSpan(DwellTime.Text) : TimeSpan.FromSeconds(DefaultDwellTimeInSeconds);
+
+            // setting up how long the geofence should be active
+            TimeSpan GeoFenceDuration;
+            GeoFenceDuration = TimeSpan.FromDays(10);
+
+            // setting up the start time of the geofence
+            DateTimeOffset GeoFenceStartTime = DateTimeOffset.Now;
+
+            geofence = new Geofence(GeofenceKey, geocircle, GeoFenceStates, singleUse, dwellTime, GeoFenceStartTime, GeoFenceDuration);
+            
+            //Add the geofence to the GeofenceMonitor
+            GeofenceMonitor.Current.Geofences.Add(geofence);
+
+            //GeofenceMonitor.Current.GeofenceStateChanged += Current_GeofenceStateChanged;
+
+        }
+
+        private async void Current_GeofenceStateChanged(GeofenceMonitor sender, object args)
+        {
+            var Reports = sender.ReadReports();
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                foreach (GeofenceStateChangeReport report in Reports)
+                {
+                    GeofenceState state = report.NewState;
+
+                    Geofence geofence = report.Geofence;
+
+                    if (state == GeofenceState.Entered)
+                    {
+                        ShowMessage("You are pretty close to your room");
+
+                    }
+                    else if (state == GeofenceState.Exited)
+                    {
+
+                        ShowMessage("You have left pretty far away from your room");
+                    }
+                }
+            });
+        }
+
+        private TimeSpan ParseTimeSpan(string DwellTimeInSeconds)
+        {
+            return TimeSpan.FromSeconds(Double.Parse(DwellTimeInSeconds));
+            
+
+        }
+
+        private static BasicGeoposition GetMyHomeLocation()
+        {
+            BasicGeoposition GeoFenceRootPosition;
+            GeoFenceRootPosition.Latitude = 23.742766;
+            GeoFenceRootPosition.Longitude = 90.417566;
+            GeoFenceRootPosition.Altitude = 0.0;
+            return GeoFenceRootPosition;
+        }
+
         private async void ShowMessage(string message)
         {
             MessageDialog dialog = new MessageDialog(message);
             await dialog.ShowAsync();
+        }
+
+        private void CreateGeoFencingButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetupGeofence();
+            
+
+        }
+
+        private async Task RegisterBackgroundTask()
+        {
+            BackgroundAccessStatus backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+
+            var geofenceTaskBuilder = new BackgroundTaskBuilder
+            {
+                Name = "My Home Geofence",
+                TaskEntryPoint = "GeofenceTask.BackgroundGeofenceTask"
+            };
+
+            var trigger = new LocationTrigger(LocationTriggerType.Geofence);
+            geofenceTaskBuilder.SetTrigger(trigger);
+
+            var geofenceTask = geofenceTaskBuilder.Register();
+            geofenceTask.Completed += GeofenceTask_Completed;
+
+            switch (backgroundAccessStatus)
+            {
+                case BackgroundAccessStatus.Unspecified:
+                case BackgroundAccessStatus.Denied:
+                    ShowMessage("This application is denied of the background task ");
+                    break;
+            }
+
+        }
+
+        private async void GeofenceTask_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            if (sender != null)
+            {
+                // Update the UI with progress reported by the background task
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    try
+                    {
+                        // If the background task threw an exception, display the exception 
+                        args.CheckResult();
+
+                        var settings = ApplicationData.Current.LocalSettings;
+
+                        // get status
+                        if (settings.Values.ContainsKey("Status"))
+                        {
+                            Status.Text = settings.Values["Status"].ToString();
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowMessage(ex.Message);
+                    }
+                });
+            }
+            
+
+        }
+
+        public static bool IsTaskRegistered(string TaskName)
+        {
+            var Registered = false;
+            var entry = BackgroundTaskRegistration.AllTasks.FirstOrDefault(keyval => keyval.Value.Name == TaskName);
+
+            if (entry.Value != null)
+                Registered = true;
+
+            return Registered;
+        }
+
+        public static void Unregister(string TaskName)
+        {
+            var entry = BackgroundTaskRegistration.AllTasks.FirstOrDefault(keyval => keyval.Value.Name == TaskName);
+
+            if (entry.Value != null)
+                entry.Value.Unregister(true);
+        }
+
+        private async void CreateBackgroundServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsTaskRegistered("My Home Geofence"))
+            {
+                Unregister("My Home Geofence");
+            }
+            await RegisterBackgroundTask();
         }
     }
 }
---
+++ Code Samples/GeoFencing in Windows Phone 8.1 WinRT/GeoFencingTest/GeofenceTask/BackgroundGeofenceTask.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Geolocation.Geofencing;
+using Windows.Storage;
+using Windows.UI.Notifications;
+
+namespace GeofenceTask
+{
+    public sealed class BackgroundGeofenceTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var Reports = GeofenceMonitor.Current.ReadReports();
+            var SelectedReport = 
+                Reports.FirstOrDefault(report => (report.Geofence.Id == "My Home Geofence") && (report.NewState == GeofenceState.Entered || report.NewState == GeofenceState.Exited));
+
+            if (SelectedReport==null)
+            {
+                return;
+            }
+
+            //We are creating a toast this time as this is running in Background
+            var ToastContent = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+
+           
+
+            var TextNodes = ToastContent.GetElementsByTagName("text");
+
+            if (SelectedReport.NewState == GeofenceState.Entered)
+            {
+                TextNodes[0].AppendChild(ToastContent.CreateTextNode("You are pretty close to your room"));
+                TextNodes[1].AppendChild(ToastContent.CreateTextNode(SelectedReport.Geofence.Id));
+            }
+            else if(SelectedReport.NewState == GeofenceState.Exited)
+            {
+                TextNodes[0].AppendChild(ToastContent.CreateTextNode("You are pretty close to your room"));
+                TextNodes[1].AppendChild(ToastContent.CreateTextNode(SelectedReport.Geofence.Id));
+            }
+
+            var settings = ApplicationData.Current.LocalSettings;
+
+            if (settings.Values.ContainsKey("Status"))
+            {
+                settings.Values["Status"] = SelectedReport.NewState;
+            }
+            else
+            {
+                settings.Values.Add(new KeyValuePair<string, object>("Status", SelectedReport.NewState.ToString()));
+            }
+
+
+            var Toast = new ToastNotification(ToastContent);
+            var ToastNotifier = ToastNotificationManager.CreateToastNotifier();
+            ToastNotifier.Show(Toast);
+
+
+        }
+    }
+}
